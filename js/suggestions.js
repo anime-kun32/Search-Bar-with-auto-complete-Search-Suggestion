2e148059b5f8143787c9dc35597b7cb66d3c3548
anime-kun32 <160499103+anime-kun32@users.noreply.github.com>
2024-04-16 15:50:41 +0100

    Update suggestions.js

--- js/suggestions.js
+++ js/suggestions.js
@@ -1,1 +1,13 @@
-let suggestions=["abc","atest","b","btest","e","etest"];
+let searchQuery = "monster"; // Search term
+let suggestions = [];
+
+// Fetch suggestions from API
+fetch(`https://api-aniwatch.onrender.com/anime/search/suggest?q=${searchQuery}`)
+  .then(response => response.json())
+  .then(data => {
+    suggestions = data.map(suggestion => suggestion.name);
+    console.log(suggestions); // Display fetched names in the console
+  })
+  .catch(error => {
+    console.error('Error fetching suggestions:', error);
+  });
